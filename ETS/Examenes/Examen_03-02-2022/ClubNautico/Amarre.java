1eee01f4edcf2e609aedda9f91bac2ee5e2ea85b
Guillermo Sicilia Hernández <92543128+GuillermoSH@users.noreply.github.com>
2022-02-18 18:22:36 +0000

    Create Amarre.java

---
+++ ETS/Examenes/Examen_03-02-2022/ClubNautico/Amarre.java
@@ -0,0 +1,10 @@
+package ClubNautico;
+
+public class Amarre {
+    private int numero;
+    private double lecContAgua;
+    private double lecContLuz;
+    private boolean servicioManten;
+    private Fecha fechaAsignacion;
+    private int ancho;
+}
