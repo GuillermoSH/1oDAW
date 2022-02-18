d81e63d465cb531ab40284122e2510d707e5cdad
Guillermo Sicilia Hernández <92543128+GuillermoSH@users.noreply.github.com>
2022-02-18 18:25:15 +0000

    Create Fecha.java

---
+++ ETS/Examenes/Examen_03-02-2022/ClubNautico/Fecha.java
@@ -0,0 +1,12 @@
+package ClubNautico;
+
+public class Fecha {
+    private int dia;
+    private int mes;
+    private int anio;
+
+    @Override
+    public String toString(){
+        return "Fecha en formato elegido";
+    }
+}
