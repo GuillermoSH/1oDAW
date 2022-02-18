30a3c97036215e00dcb753af7a986524b4f8bfcd
Guillermo Sicilia Hernández <92543128+GuillermoSH@users.noreply.github.com>
2022-02-18 18:24:54 +0000

    Create Empleado.java

---
+++ ETS/Examenes/Examen_03-02-2022/ClubNautico/Empleado.java
@@ -0,0 +1,10 @@
+package ClubNautico;
+
+public class Empleado {
+    private int codigo;
+    private String nombre;
+    private DireccionTlf direccion;
+    private DireccionTlf telefono;
+    private String especialidad;
+    private Zona numBarcosEncarga;
+}
