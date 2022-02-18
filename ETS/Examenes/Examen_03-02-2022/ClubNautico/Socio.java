ff999561bbb2d4d57bbfb12bb4cb3511f93220d6
Guillermo Sicilia Hernández <92543128+GuillermoSH@users.noreply.github.com>
2022-02-18 18:25:43 +0000

    Create Socio.java

---
+++ ETS/Examenes/Examen_03-02-2022/ClubNautico/Socio.java
@@ -0,0 +1,9 @@
+package ClubNautico;
+
+public class Socio {
+    private String nombre;
+    private DireccionTlf direccion;
+    private String DNI;
+    private DireccionTlf telefono;
+    private Fecha fechaIngreso;
+}
