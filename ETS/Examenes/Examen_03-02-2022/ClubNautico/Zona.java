86844bed9ca4a688de490833d85f4d1c93006e05
Guillermo Sicilia Hernández <92543128+GuillermoSH@users.noreply.github.com>
2022-02-18 18:26:13 +0000

    Create Zona.java

---
+++ ETS/Examenes/Examen_03-02-2022/ClubNautico/Zona.java
@@ -0,0 +1,9 @@
+package ClubNautico;
+
+public class Zona {
+    private char codigo;
+    private String tipoBarcos;
+    private int numBarcos;
+    private double profundidad;
+    private Amarre anchoAmarre;
+}
