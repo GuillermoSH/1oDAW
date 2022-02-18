ab5fb2928474f42f27106ea2bc63c4fd1c2d25b8
Guillermo Sicilia Hernández <92543128+GuillermoSH@users.noreply.github.com>
2022-02-18 18:24:38 +0000

    Create Embarcacion.java

---
+++ ETS/Examenes/Examen_03-02-2022/ClubNautico/Embarcacion.java
@@ -0,0 +1,8 @@
+package ClubNautico;
+
+public class Embarcacion {
+    private String matricula;
+    private String nombre;
+    private String tipo;
+    private String dimensiones; //String considerando el formato 10x12x200
+}
