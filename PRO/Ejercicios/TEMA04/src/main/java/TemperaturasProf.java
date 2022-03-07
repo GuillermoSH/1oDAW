b34f92301febceba5ccd3529c0188acfdf0b7131
Guillermo Sicilia Hernández <92543128+GuillermoSH@users.noreply.github.com>
2022-03-07 18:29:22 +0000

    Update TemperaturasProf.java
    PRO 07-03-2022

--- PRO/Ejercicios/TEMA04/src/main/java/TemperaturasProf.java
+++ PRO/Ejercicios/TEMA04/src/main/java/TemperaturasProf.java
@@ -36,18 +36,51 @@ public class TemperaturasProf {
         return suma/datos[mes].length;
     }
     
-    public void imprimeMedias(){
+    public String nombreMes(int m){
         String[] meses=new String[]{"Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"};
-
+        
+        return meses[m];
+    }
+        
+    public void imprimeMedias(){
         for(int mes=0;mes<datos.length;mes++){
-            System.out.printf("La media del mes de %s es %.2f",meses[mes],this.temperaturaMedia(mes));
+            System.out.printf("La media del mes de %s es %.2f",nombreMeses(mes),this.temperaturaMedia(mes));
         }
     }
-
+    
+    public int[] diasMasExtremos(int mes){
+        int indMin=0,indMax=0;
+        
+        for(int i=1;i<datos[mes].length;i++){
+            if(datos[mes][i]<datos[mes][indMin]){
+                indMin=i;
+            }
+            if(datos[mes][i]>datos[mes][indMin]){
+                indMax=i;
+            }
+        }
+        return new int[]{indMin+1,indMax+1};
+    }
+    
+    public void imprimeTemperaturasExtremas(){
+        int[] datosMes;
+        
+        for(int i=0;i<datos.length;i++){
+            datosMes=diasMasExtremos(i);
+            System.out.printf("El día mas frio de %s fue %d.\n",nombreMes(i),datosMes[0]);
+            System.out.printf("El día mas caluroso de %s fue %d.\n",nombreMes(i),datosMes[1]);
+        }
+    }
+    
+    public void imprimeDiasExtremosAño(){
+        
+    }
+    
     public static void main(String args[]) throws FileNotFoundException{
         TemperaturasProf a2019=new TemperaturasProf("rutaAbsoluta");
         a2019.leerDatos();
         //double m=a2019.temperaturaMedia(0);
-        a2019.imprimeMedias();
+        //a2019.imprimeMedias();
+        a2019.imprimeTemperaturasExtremas();
     }
 }
